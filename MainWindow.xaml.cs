8b7274dca283b3021bbb8c545631fd2c7cea7c5f
Julia Isaksson <julia.jenny.isaksson@gmail.com>
2025-07-20 21:00:04 +0200

    Adding comments to the code to inform the reader what main objectives of the assignment and its description

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,47 @@
-﻿using System.Text;
+﻿using Microsoft.VisualBasic;
+using Microsoft.Win32;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.CompilerServices;
 
 namespace Assignment5V25
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    //Assignment 6 
+
+    //////////////////////////////////////////////////
+    // The main objectives of this assignment are //
+    ////////////////////////////////////////////////
+
+    //• To use records to define immutable data structures.
+
+    //• To use tuples to return multiple values from methods.
+
+    //• To practice working with collections (e.g., Lists, Dictionaries, and HashSets) to manage and organize data.
+
+    //////////////////
+    // Description //
+    ////////////////
+
+    // We are going to simulate a simple use case of flight departures in an airport.
+    // The airport has a control tower.A controller registers a plane to get ready on the runway and
+    // to take off using a software with a UI that may be designed as illustrated in the figure below.
+    // The top ListBox lists the planes added in a list of airplanes handled by the Control Tower while
+    // the lower ListBox is used to display the flight status and other information.The user selects
+    // an airplane in the top ListBox before clicking the Take.
+
     public partial class MainWindow : Window
     {
         private ControlTower tower;
